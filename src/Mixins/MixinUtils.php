7059a6146c66c602080c46462de6679fe6cab6ed
Luca Patera <lucapatera@outlook.it>
2023-06-01 19:34:46 +0200

    Fix Mixins

--- src/Mixins/MixinUtils.php
+++ src/Mixins/MixinUtils.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Str;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use Psr\Http\Client\ClientInterface;
+use RuntimeException;
 use SergiX44\Nutgram\Telegram\Types\Media\File;
 
 class MixinUtils
@@ -24,22 +25,28 @@ class MixinUtils
      */
     public static function saveFileToDisk(File $file, string $path, ?string $disk = null, array $clientOpt = []): bool
     {
+        $bot = $file->getBot();
+
+        if ($bot === null) {
+            throw new RuntimeException('Bot instance not found.');
+        }
+
         $storage = Storage::disk($disk);
 
         if (Str::endsWith($path, ['/', '\\'])) {
             $path .= basename($file->file_path ?? $file->file_id);
         }
 
-        if ($file->getConfig()->is_local ?? false) {
-            return $storage->put($path, $file->downloadUrl($file));
+        if ($bot->getConfig()->is_local ?? false) {
+            return $storage->put($path, $bot->downloadUrl($file));
         }
 
         //create temp file
-        $tmpFile = tempnam(sys_get_temp_dir(), uniqid(strftime('%G-%m-%d'), true));
+        $tmpFile = tempnam(sys_get_temp_dir(), uniqid(time(), true));
 
         //download file to temp file
-        $http = $file->getContainer()->get(ClientInterface::class);
-        $http->get($file->downloadUrl($file), array_merge(['sink' => $tmpFile], $clientOpt));
+        $http = $bot->getContainer()->get(ClientInterface::class);
+        $http->get($bot->downloadUrl($file), array_merge(['sink' => $tmpFile], $clientOpt));
 
         //save temp file to disk
         return $storage->putFileAs('/', new LaravelFile($tmpFile), $path);
