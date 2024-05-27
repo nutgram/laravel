d950d0cf3abb75202bb5737586992e0e3acf88bc
Sergio Brighenti <sergio@brighenti.me>
2024-05-27 11:25:27 +0200

    fix

--- src/Console/ListenCommand.php
+++ src/Console/ListenCommand.php
@@ -3,6 +3,7 @@
 namespace Nutgram\Laravel\Console;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\Facades\App;
 use SergiX44\Nutgram\Nutgram;
 use SergiX44\Nutgram\RunningMode\SingleUpdate;
@@ -13,7 +14,7 @@ class ListenCommand extends Command
 
     protected $description = 'Start the bot for development and reloads after every update.';
 
-    public function handle(): void
+    public function handle(Container $container): void
     {
         while (true) {
             App::forgetInstance(Nutgram::class);
