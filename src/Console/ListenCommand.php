46f10bf07e941f65c7bef1911b5ae2eedf95f776
Sergio Brighenti <sergio@brighenti.me>
2025-06-12 22:38:01 +0200

    update ListenCommand for development mode and adjust nutgram dependency

--- src/Console/ListenCommand.php
+++ src/Console/ListenCommand.php
@@ -8,14 +8,21 @@ use SergiX44\Nutgram\RunningMode\SingleUpdate;
 
 class ListenCommand extends Command
 {
-    protected $signature = 'nutgram:listen';
+    protected $signature = 'nutgram:listen {--pollingTimeout=1}';
 
     protected $description = 'Start the bot for development and reloads after every update.';
 
     public function handle(): void
     {
+        $this->warn('This running mode is very inefficient and only suitable for development purposes. DO NOT USE IN PRODUCTION!');
         $this->info('Listening...');
+
+        $pollingTimeout = $this->option('pollingTimeout') ?: 1;
+        config()?->set('nutgram.config.polling.timeout', $pollingTimeout);
         while (true) {
+            if (function_exists('opcache_reset')) {
+                opcache_reset();
+            }
             app()->forgetInstance(Nutgram::class);
             $bot = app(Nutgram::class);
             $bot->setRunningMode(SingleUpdate::class);
